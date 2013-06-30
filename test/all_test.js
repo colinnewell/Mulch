4459a83fc31756b7677c221e9dd15bb60e5d1c20
Colin Newell <colin.newell@gmail.com>
2013-06-30 11:01:43 +0100

    Started to add code for view tests

--- test/all_test.js
+++ test/all_test.js
@@ -1,12 +1,21 @@
-var tap = require('tap')
+var tap = require('tap');
 
 tap.test('The app loads', function(t) {
-  t.doesNotThrow(load_app, 'No problem loading the app.js file')
-  t.end()
+  t.doesNotThrow(load_app, 'No problem loading the app.js file');
+  t.end();
 
   function load_app() {
-    var app = require('../lib/app')
+    var app = require('../lib/app');
   }
-})
+});
 
-
+tap.test('View test', function(t) {
+    emit = function(key, doc) {
+        t.comment(key + ":" + doc);
+    };
+    var app = require('../lib/app');
+    var doc = {};
+    app.views.all.map({type: "openerp_log"});
+    var doc = {};
+    t.end();
+});
